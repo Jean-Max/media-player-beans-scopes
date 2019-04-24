4fcefccafdf86f06aa374b6bfce1637bd2de8d20
Jean-Max Marie-Sainte <jean-max-mariesainte@viseo.com>
2019-04-24 17:30:33 +0200

    Fix compilation issue

--- src/main/java/com/viseo/formation/config/MediaFileConfig.java
+++ src/main/java/com/viseo/formation/config/MediaFileConfig.java
@@ -1,13 +1,10 @@
 package com.viseo.formation.config;
 
 import com.viseo.formation.domain.MediaFile;
-import com.viseo.formation.domain.MediaFileGenerator;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 public class MediaFileConfig {
@@ -22,17 +19,5 @@ public class MediaFileConfig {
     @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     MediaFile personPrototype(){
         return new MediaFile();
-    }
-
-    @Bean(name ="mediaFileRequest")
-    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-    MediaFileGenerator mediaFileRequest(){
-        return new MediaFileGenerator();
-    }
-
-    @Bean(name ="mediaFileSession")
-    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-    MediaFileGenerator mediaFileSession(){
-        return new MediaFileGenerator();
     }
 }
